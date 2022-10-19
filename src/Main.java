8588b171f89e56698619736fd5c91ef446cfb51b
Doct0rWh0 <lefik123@bk.ru>
2022-10-19 10:14:03 +0300

    Pushing SRC files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
+        public static void main(String[] args) {
+            MyRunnable myRunnable = new MyRunnable();
+            Thread[] threadArr = new Thread[5];
+            
+            Thread thread1 = new Thread(myRunnable);
+            Thread thread2 = new Thread(myRunnable);
+            Thread thread3 = new Thread(myRunnable);
 
-    public static void main(String[] args) {
-        
-    }
+            thread1.start();
+            thread2.start();
+            thread3.start();
+        }
 }
+
---
+++ src/MyThread.java
@@ -0,0 +1,8 @@
+public class MyThread extends Thread {
+    //    public void run() {
+//        System.out.println("Important job running in MyThread");
+//    }
+    public void run(String s) {
+        System.out.println("String in run is " + s);
+    }
+}
